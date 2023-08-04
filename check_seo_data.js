f2c9d373bea0980230cbea33b0becece8c508be1
Tim Berlin <49077932+tberlin-om@users.noreply.github.com>
2023-08-04 12:14:46 +0200

    Update check_seo_data.js

--- check_seo_data.js
+++ check_seo_data.js
@@ -40,3 +40,37 @@ function countOutlinks() {
     }
   });
 }
+
+function checkResponseTimes() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var responseTimesSheet = spreadsheet.getSheetByName("Response Times");
+
+  if (!responseTimesSheet) {
+    responseTimesSheet = spreadsheet.insertSheet("Response Times");
+  }
+
+  var currentDate = new Date().toLocaleDateString("de-DE");
+  var lastResponseTimeColumn = responseTimesSheet.getLastColumn() + 1;
+
+  var urls = responseTimesSheet.getRange(2, 1, responseTimesSheet.getLastRow() - 1, 1).getValues();
+
+  urls.forEach(function(url, index) {
+    url = url[0];
+    if (url) {
+      try {
+        var startTime = new Date().getTime();
+        var response = UrlFetchApp.fetch(url);
+        var endTime = new Date().getTime();
+
+        var responseTime = endTime - startTime;
+
+        responseTimesSheet.getRange(1, lastResponseTimeColumn).setValue(currentDate);
+        responseTimesSheet.getRange(index + 2, lastResponseTimeColumn).setValue(responseTime);
+      } catch (error) {
+        console.error("Fehler beim Abrufen der URL " + url + ": " + error.message);
+
+        responseTimesSheet.getRange(index + 2, lastResponseTimeColumn).setValue("Fehler");
+      }
+    }
+  });
+}
