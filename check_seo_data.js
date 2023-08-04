2f4180c46918c0dc4641d38490f8f79075cc1e92
Tim Berlin <49077932+tberlin-om@users.noreply.github.com>
2023-08-04 12:14:17 +0200

    Update check_seo_data.js

--- check_seo_data.js
+++ check_seo_data.js
@@ -1,1 +1,42 @@
+function countOutlinks() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var outlinksSheet = spreadsheet.getSheetByName("Outlinks");
+  var uniqueOutlinksSheet = spreadsheet.getSheetByName("Unique Outlinks");
 
+  if (!outlinksSheet) {
+    outlinksSheet = spreadsheet.insertSheet("Outlinks");
+  }
+  if (!uniqueOutlinksSheet) {
+    uniqueOutlinksSheet = spreadsheet.insertSheet("Unique Outlinks");
+  }
+
+  var currentDate = new Date().toLocaleDateString("de-DE");
+  var lastOutlinkColumn = outlinksSheet.getLastColumn() + 1;
+  var lastUniqueOutlinkColumn = uniqueOutlinksSheet.getLastColumn() + 1;
+
+  var urls = outlinksSheet.getRange(2, 1, outlinksSheet.getLastRow() - 1, 1).getValues();
+
+  urls.forEach(function(url, index) {
+    url = url[0];
+    if (url) {
+      try {
+        var html = UrlFetchApp.fetch(url).getContentText();
+        var outlinks = html.match(/<a [^>]*href="(\/[^\/"][^"]*|https:\/\/www\.aponeo\.de[^"]*)"/g);
+        if (outlinks) {
+          var uniqueOutlinks = [...new Set(outlinks)];
+
+          outlinksSheet.getRange(1, lastOutlinkColumn).setValue(currentDate);
+          outlinksSheet.getRange(index + 2, lastOutlinkColumn).setValue(outlinks.length);
+
+          uniqueOutlinksSheet.getRange(1, lastUniqueOutlinkColumn).setValue(currentDate);
+          uniqueOutlinksSheet.getRange(index + 2, lastUniqueOutlinkColumn).setValue(uniqueOutlinks.length);
+        }
+      } catch (error) {
+        console.error("Fehler beim Abrufen der URL " + url + ": " + error.message);
+
+        outlinksSheet.getRange(index + 2, lastOutlinkColumn).setValue("Fehler");
+        uniqueOutlinksSheet.getRange(index + 2, lastUniqueOutlinkColumn).setValue("Fehler");
+      }
+    }
+  });
+}
